ee1ae58ab01ee5342f887821c952486e0844180f
GILANG-PC\gilan <gilangramadhan96.gr@gmail.com>
2018-10-27 14:25:25 +0700

    tambah nomor pemadam

--- app/src/main/java/org/sandec/emergencyapp/SettingActivity.java
+++ app/src/main/java/org/sandec/emergencyapp/SettingActivity.java
@@ -15,10 +15,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
     //kenalin
-    EditText nomerPolisi;
+    EditText nomerPolisi, nomorPemadam;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,16 +27,22 @@ public class SettingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         nomerPolisi= findViewById(R.id.edtNomorPolisi);
+        nomorPemadam = findViewById(R.id.edtNomorPemadam);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String polisi = nomerPolisi.getText().toString();
-                if (polisi.isEmpty()){
+                String pemadam = nomorPemadam.getText().toString();
+                if (polisi.isEmpty()) {
                     nomerPolisi.setError("Tidak boleh kosong");
+                }else if (pemadam.isEmpty()){
+                    nomorPemadam.setError("Tidak Boleh Kosong");
                 }else {
                     //esekusi
                     Helper.Save(SettingActivity.this, Helper.NOMOR_POLISI, polisi);
+                    Helper.Save(SettingActivity.this, Helper.NOMOR_PEMADAM, pemadam);
+                    Toast.makeText(SettingActivity.this, "Sukses Simpan", Toast.LENGTH_SHORT).show();
                 }
             }
         });
