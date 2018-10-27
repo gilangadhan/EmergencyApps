f3f4cf16d56a091f24882563f79c1fee805ee03d
GILANG-PC\gilan <gilangramadhan96.gr@gmail.com>
2018-10-27 14:07:02 +0700

    updatehelper

--- app/src/main/java/org/sandec/emergencyapp/Helper.java
+++ app/src/main/java/org/sandec/emergencyapp/Helper.java
@@ -11,14 +11,19 @@ import android.preference.PreferenceManager;
 
 public class Helper {
 
-    public final void Save(Context context, String key, String value) {
+    public static final String NOMOR_POLISI = "nomor_polisi";
+    public static final String NOMOR_AMBULAN = "nomor_ambulan";
+    public static final String NOMOR_PEMADAM = "nomor_pemadam";
+    public static final String NOMOR_BASARNAS = "nomor_basarnas";
+
+    public static void Save(Context context, String key, String value) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(key, value);
         editor.apply();
     }
 
-    public final String Read(Context context, String key) {
+    public static String Read(Context context, String key) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         return sharedPreferences.getString(key, "");
     }
