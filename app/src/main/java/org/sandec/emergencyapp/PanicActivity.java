1a48d679686cd6d09812a3f5add78920a4794eba
GILANG-PC\gilan <gilangramadhan96.gr@gmail.com>
2018-10-27 15:58:10 +0700

    update location

--- app/src/main/java/org/sandec/emergencyapp/PanicActivity.java
+++ app/src/main/java/org/sandec/emergencyapp/PanicActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,18 +21,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-public class PanicActivity extends AppCompatActivity {
+public class PanicActivity extends AppCompatActivity implements LocationListener {
 
-    LocationManager locationManager;
-    Criteria criteria;
+    protected LocationManager locationManager;
+    double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panic);
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        criteria = new Criteria();
 
         findViewById(R.id.btnMenu).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,13 +41,18 @@ public class PanicActivity extends AppCompatActivity {
         findViewById(R.id.imgPanic).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ActivityCompat.checkSelfPermission(PanicActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(PanicActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-                    ActivityCompat.requestPermissions(PanicActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_CHECKIN_PROPERTIES}, 100);
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(PanicActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                            100);
                     return;
-
                 }
-                Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                String bestProvider = String.valueOf(locationManager.getBestProvider(new Criteria(), true));
+                Location location = locationManager.getLastKnownLocation(bestProvider);
                 sendPanic(location);
             }
         });
@@ -57,13 +60,86 @@ public class PanicActivity extends AppCompatActivity {
 
     private void sendPanic(Location location) {
         String nomerOrangPenting = Helper.Read(PanicActivity.this, Helper.NOMOR_POLISI);
-        if (nomerOrangPenting.isEmpty()){
+        if (nomerOrangPenting.isEmpty()) {
             Toast.makeText(this, "Masukkan nomer di setting", Toast.LENGTH_SHORT).show();
-        }else{
-            String message = String.format("Hallo bro, aku lagi kena musibah. Lokasiku di titik ini %s,%s", location.getLatitude(), location.getLongitude());
-            Intent kirimSms = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+nomerOrangPenting));
-            kirimSms.putExtra("sms_body", message);
-            startActivity(kirimSms);
+        } else {
+            if (location != null) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                String message = "Hallo bro, aku lagi kena musibah. Lokasiku di titik ini " + location.getLatitude() + "," + location.getLongitude();
+                Intent kirimSms = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + nomerOrangPenting));
+                kirimSms.putExtra("sms_body", message);
+                startActivity(kirimSms);
+            }else{
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(PanicActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                            100);
+                    return;
+                }
+               String bestProvider = String.valueOf(locationManager.getBestProvider(new Criteria(), true));
+                locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+            }
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 100) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                            100);
+                    return;
+                }
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                sendPanic(location);
+
+            } else {
+
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //remove location callback:
+        locationManager.removeUpdates(this);
+
+        //open the map:
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
